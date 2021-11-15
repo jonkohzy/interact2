7f9f09c67d5f10391085c03a5332099e9f6aed28
theoleecj <theodorelcj@gmail.com>
2021-11-16 06:04:04 +0800

    2 - shim overlayscrollbars library

---
+++ js/OverlayScrollbars.js
@@ -0,0 +1,3 @@
+// there is no longer a need for this library as thin scrollbars are now a Web Standard.
+// therefore, it's just shimmed with a fake function now
+function OverlayScrollbars(a, b) { }
